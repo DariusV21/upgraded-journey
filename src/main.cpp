7d06e9ee9bf6b99c9850ecd6ff738fbdec44f6c2
Darius Vaitiekus <dvaitiekus@vejas.lan>
2018-01-12 14:17:26 +0200

    LCD ekranas rodo BTC

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <Adafruit_GFX.h>    // Core graphics library
 #include <XTronical_ST7735.h> // Hardware-specific library
 #include <SPI.h>
+#include <Keyboard.h>
 
 // set up pins we are going to use to talk to the screen
 #define TFT_SCLK 13         // SPI clock (SCK)
@@ -47,40 +48,217 @@ Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS,  TFT_DC, TFT_RST);
 float p = 3.1415926;
 
 
+
+word ConvertRGB(byte R, byte G, byte B)
+{
+  return ( ((R & 0xF8) << 8) | ((G & 0xFC) << 3) | (B >> 3) );
+}
+
 void textOnScreen(char *text) {
   tft.fillScreen(ST7735_BLACK);
   tft.setCursor(10, 30);
-  tft.setTextColor(ST7735_RED);
+  tft.setTextColor(ConvertRGB(0, 153, 255));
   tft.setTextSize(1);
   tft.println(text);
+  delay(500);
+}
+
+
+void drawCircle(float lineWidth, word lineColor){
+  word backgroundColor = ConvertRGB(0, 0, 0);
+  tft.fillScreen(backgroundColor);
+  // tft.drawCircle(tft.width()/2, tft.height()/2, 10, lineColor);
+  // delay(1000);
+  tft.fillCircle(32, 48, 20+(lineWidth/2), lineColor);
+  tft.fillCircle(32, 48, 20-(lineWidth/2), backgroundColor);
+  // tft.drawCircle(96, 48, 20, lineColor);
   delay(1000);
 }
 
-void drawCircles(float lineWidth){
+void drawRoundRectangle(float x0, float y0, uint16_t w, uint16_t h, uint16_t radius,
+    float lineWidth, uint16_t color){
+      word backgroundColor = ConvertRGB(0, 0, 0);
+      tft.fillScreen(backgroundColor);
+      tft.fillRoundRect(x0-(lineWidth/2), y0-(lineWidth/2), w+lineWidth, h+lineWidth, radius+lineWidth, color);
+      tft.fillRoundRect(x0+(lineWidth/2), y0+(lineWidth/2), w-lineWidth, h-lineWidth, radius-lineWidth, backgroundColor);
+      tft.drawRoundRect(10, 60, 100, 50, 10, ConvertRGB(0, 153, 255));
+      delay(1000);
+}
+
+
+void slidingColor(){
   tft.fillScreen(ST7735_BLACK);
-  tft.drawCircle(tft.width()/2, tft.height()/2, 10, ST7735_GREEN);
-  delay(1000);
+for (byte posY = 0; posY < 128; posY++) {
+  for (byte posX = 0; posX < 128; posX++) {
+    tft.drawPixel(posX, posY, ST7735_WHITE);
+  }
+}
+for (byte posY = 0; posY < 128; posY++) {
+  for (byte posX = 0; posX < 128; posX++) {
+    tft.drawPixel(posX, posY, ST7735_BLACK);
+  }
+}
+
+  // for (byte r = 0; r < 255; r++) {
+  //   for (byte g = 0; g < 255; g++) {
+  //     for (byte b = 0; b < 255; b++) {
+  //       // tft.fillScreen(ConvertRGB(r, g, b));
+  //       tft.drawPixel(r/2, r/2, (ConvertRGB(r, g, b)));
+  //     }
+  //   }
+  // }
+}
+
+void colorTest1(){
+  for (byte posY = 0; posY < 128; posY++) {
+    for (byte posX = 0; posX < 128; posX++) {
+      if (posX == 64) {
+        tft.drawPixel(posX, posY, ST7735_GREEN);
+      } else {
+        tft.drawPixel(posX, posY, ConvertRGB(posX*2, posY*2, posY*2));
+      }
+
+    }
+  }
+  delay(3000);
+}
+void colorTest2(){
+  for (byte posY = 0; posY < 128; posY++) {
+    for (byte posX = 0; posX < 128; posX++) {
+      if (posY == 64) {
+        tft.drawPixel(posX, posY, ST7735_RED);
+      } else {
+        tft.drawPixel(posX, posY, ConvertRGB(posX*2, posX*2, posY*2));
+      }
+    }
+  }
+  delay(3000);
+}
+void colorTest3(){
+  for (byte posY = 0; posY < 128; posY++) {
+    for (byte posX = 0; posX < 128; posX++) {
+      if (posX == 64) {
+        tft.drawPixel(64, posY, ST7735_BLUE);
+      } else {
+        tft.drawPixel(posX, posY, ConvertRGB(posX*2, posY*2, posX*2));
+      }
+      if (posY == 64) {
+        tft.drawPixel(posX, 64, ST7735_BLUE);
+      }
+    }
+  }
+  delay(3000);
+}
+void colorTest4(){
+  for (byte posY = 0; posY < 128; posY++) {
+    for (byte posX = 0; posX < 128; posX++) {
+
+      tft.drawPixel(posX, posY, ConvertRGB(posY*2, posY*2, posY*2));
+    }
+  }
+  delay(3000);
+}
 
 
-  // tft.drawCircle(32, 48, 20, ST7735_RED);
-  tft.fillCircle(32, 48, 20+(lineWidth/2), ST7735_RED);
-  // tft.drawCircle(32, 48, 20-(lineWidth/2), ST7735_RED);
-  tft.fillCircle(32, 48, 20-(lineWidth/2), ST7735_BLACK);
-
-  tft.drawCircle(96, 48, 20, ST7735_RED);
+// drawEye(x,y){
+//   byte nX = x
+//   drawRoundRectangle(10, 0, 100, 50, 10, 4, ConvertRGB(0, 153, 255));
+// }
 
 
+char msg[10];
+int z = 0;
+bool readNextChar = false;
 
-  delay(5000);
+
+
+void writeBTC(){
+  tft.fillScreen(ST7735_Black);
+  tft.setTextColor(ST7735_White);
+  tft.setCursor(10, 25);
+  tft.setTextSize(2);
+
+  tft.print(msg);
 }
 
 
 
 void setup(void) {
+  Serial.begin(9600);
+
   tft.init();   // initialize a ST7735S chip,
+  tft.setRotation(3);
+  tft.fillScreen(ST7735_BLACK);
+  // word color = ST7735_White;
+
 }
 
 void loop() {
-  textOnScreen("Platformio!");
-  drawCircles(5);
+
+  while (Serial.available() > 0) {
+    char c = Serial.read();
+    int i = (int)c;
+
+
+
+    if (i == 36) { // $
+      readNextChar = true;
+    } else if (i == 37) { // %
+        readNextChar = false;
+        z = 0;
+        writeBTC();
+        Serial.print(msg);
+    } else if (readNextChar) {
+        msg[z] = c;
+        z++;
+        if (z >= 10) z = 0;
+    }
+
+
+
+
+
+
+
+
+    // if(i == 36) { //$
+    //   tft.fillScreen(ST7735_Black);
+    //   tft.setTextColor(ST7735_White);
+    //   tft.setCursor(10, 25);
+    //   tft.setTextSize(2);
+    // } else {
+    //   Serial.print(c);
+    //   tft.print(c);
+    // }
+    // if(i == 37) { //%
+    // tft.setTextColor(ST7735_Green);
+    // }
+    // if(i == 51) {
+    // tft.setTextColor(ST7735_White);
+    // }
+
+
+
+
+    // if(i == 48) {
+    //   Serial.println("REFRESH");
+    //   tft.fillScreen(ST7735_BLACK);
+    //   tft.setCursor(0, 0);
+    // } else {
+    //   Serial.print(c);
+    //   tft.print(c);
+    // }
+
+
+
+  }
+  // textOnScreen("Platformio!");
+  // drawCircle(5, ConvertRGB(0, 153, 255));
+  // drawRoundRectangle(10, 0, 100, 50, 10, 4, ConvertRGB(0, 153, 255));
+  // slidingColor();
+  // colorTest1();
+  // colorTest2();
+  // colorTest3();
+  // colorTest4();
+
 }
